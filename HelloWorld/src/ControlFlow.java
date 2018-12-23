b362e04a2269af7f51a4709b28e4cea66163b42e
Yael Brown <yaelrbrown@gmail.com>
2018-12-23 01:21:16 -0500

    Started control flow
    Finished overloading methods and started control flow

---
+++ HelloWorld/src/ControlFlow.java
@@ -0,0 +1,69 @@
+public class ControlFlow {
+
+
+
+    public static void main(String[] args) {
+        int switchValue = 3;
+        char charValue = 'A';
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("was a 3, or a 4, or a 5");
+                System.out.println("Actually it was " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+        switch(charValue) {
+            case 'A':
+                System.out.println("The char is: A");
+                break;
+
+            case 'B':
+                System.out.println("The char is: B");
+                break;
+
+            case 'C':
+                System.out.println("The char is: C");
+                break;
+
+            case 'D':
+                System.out.println("The char is: D");
+                break;
+
+            case 'E':
+                System.out.println("The char is: E");
+                break;
+
+            default:
+                System.out.println("Letter not found");
+                break;
+        }
+
+        // use .toLowerCase() method to convert any combination of January, jAnuary, jaNuary to be validated in switch statement.
+        String month = "January";
+
+        switch(month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("not sure");
+                break;
+        }
+    }
+}
