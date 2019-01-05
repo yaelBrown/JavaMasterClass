7412ce09c1de224202c52b8c15fec4c972b6430c
Yael Brown <yaelrbrown@gmail.com>
2019-01-05 09:28:27 -0500

    Removed white space
    Removed whitespace in overloading methods.

--- HelloWorld/src/OverLoadingMethods.java
+++ HelloWorld/src/OverLoadingMethods.java
@@ -7,8 +7,6 @@ public class OverLoadingMethods {
         calculateScore(750);
     }
 
-
-
     public static int calculateScore(String playerName, int score) {
             System.out.println("Player " + playerName + " scored " + score + " points");
             return score * 1000;
