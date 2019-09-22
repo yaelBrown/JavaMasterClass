0c675f4140ce18d7730d5238a669006e6e10509b
Yael Brown <yaelrbrown@gmail.com>
2019-09-21 22:24:24 -0500

    Finished on control flow statements, started OOP. Internet running slow pushing and calling it a night.

--- HelloWorld/src/ControlFlowifElse.java
+++ HelloWorld/src/ControlFlowifElse.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class ControlFlowifElse {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is " + name);
+
+        // closes the scanner
+        scanner.close();
+
+        
 
     }
 
